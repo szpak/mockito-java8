92defe3be56566aa2cbe910951847348cf8baf1c
Marcin Zajaczkowski <szpak@users.sourceforge.net>
2015-06-25 23:33:13 +0200

    Tests for LambdaMatcher in stubbing

---
+++ src/test/java/info/solidsoft/mockito/java8/LambdaMatcherStubbingTest.java
@@ -0,0 +1,69 @@
+package info.solidsoft.mockito.java8;
+
+import info.solidsoft.mockito.java8.domain.ShipSearchCriteria;
+import info.solidsoft.mockito.java8.domain.TacticalStation;
+import org.junit.Rule;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+import org.mockito.stubbing.Answer;
+
+import static info.solidsoft.mockito.java8.LambdaMatcher.argLambda;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Matchers.any;
+
+public class LambdaMatcherStubbingTest {
+
+    @Rule
+    public MockitoRule mockitoRule = MockitoJUnit.rule();
+
+    @Mock
+    private TacticalStation ts;
+
+    @Test
+    public void shouldAllowToUseLambdaInStubbing() {
+        //given
+        given(ts.findNumberOfShipsInRangeByCriteria(argLambda(c -> c.getMinimumRange() > 1000))).willReturn(4);
+        //expect
+        assertThat(ts.findNumberOfShipsInRangeByCriteria(new ShipSearchCriteria(1500, 2))).isEqualTo(4);
+        //expect
+        assertThat(ts.findNumberOfShipsInRangeByCriteria(new ShipSearchCriteria(700, 2))).isEqualTo(0);
+    }
+
+    @Test
+    public void stubbingWithCustomAnswerShouldBeLonger() {
+        //given
+        given(ts.findNumberOfShipsInRangeByCriteria(any())).willAnswer(new Answer<Integer>() {
+            @Override
+            public Integer answer(InvocationOnMock invocation) throws Throwable {
+                Object[] args = invocation.getArguments();
+                ShipSearchCriteria criteria = (ShipSearchCriteria) args[0];
+                if (criteria.getMinimumRange() > 1000) {
+                    return 4;
+                } else {
+                    return 0;
+                }
+            }
+        });
+        //expect
+        assertThat(ts.findNumberOfShipsInRangeByCriteria(new ShipSearchCriteria(1500, 2))).isEqualTo(4);
+        //expect
+        assertThat(ts.findNumberOfShipsInRangeByCriteria(new ShipSearchCriteria(700, 2))).isEqualTo(0);
+    }
+
+    @Test
+    public void stubbingWithCustomAnswerShouldBeLongerEvenAsLambda() {
+        //given
+        given(ts.findNumberOfShipsInRangeByCriteria(any())).willAnswer(invocation -> {
+            ShipSearchCriteria criteria = (ShipSearchCriteria) invocation.getArguments()[0];
+            return criteria.getMinimumRange() > 1000 ? 4 : 0;
+        });
+        //expect
+        assertThat(ts.findNumberOfShipsInRangeByCriteria(new ShipSearchCriteria(1500, 2))).isEqualTo(4);
+        //expect
+        assertThat(ts.findNumberOfShipsInRangeByCriteria(new ShipSearchCriteria(700, 2))).isEqualTo(0);
+    }
+}
